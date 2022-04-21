2db655ea5fd520522509164574ae8b2bf141ea44
MohammadAli DaneshSefatDoust <72104345+MohammadAliDaneshSefatDoust@users.noreply.github.com>
2022-04-21 17:35:51 +0430

    Add files via upload

---
+++ 1- inputs.m
@@ -0,0 +1,9 @@
+Fin=5.49621582*10^6;              %input frequency
+N_sample=32768;                   %number of samples
+Fs=100000000;                     %sampling rate
+Ts=(1/100000000);                 %TS=1/sampling rate
+t=(0:1:32767).*Ts;                %time
+Vin=1*sin(2*pi*Fin*t);            %input
+Numbits=15;
+FSV=2*1;                          %Full Scale Voltage
+LSB=(FSV)/(2^Numbits);            %Least Significant Bit
